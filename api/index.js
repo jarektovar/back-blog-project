aadf103fccd6c8b63aac1b771cf0298fbf2fad24
Santiago Andrade Mesa <andrade11mesa@gmail.com>
2024-06-08 23:32:48 -0500

    Connection to database - local mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log('Base de datos conectada')
+})
+.catch((err)=>{
+    console.log(err);
+})
 const app = express();
+
 
 app.listen(3000, ()=>{
     console.log('Probando server en el puerto 3000 xd')
