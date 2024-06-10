3b80853ec27f3c5f8db8b614164f7e695936167d
Santiago Andrade Mesa <andrade11mesa@gmail.com>
2024-06-09 21:50:17 -0500

    Add middleware index

--- api/index.js
+++ api/index.js
@@ -1,25 +1,37 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import {config} from 'dotenv'
-import userRoutes from './routes/user.route.js'
-import authRoutes from './routes/auth.route.js'
+import express from "express";
+import mongoose from "mongoose";
+import { config } from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 config();
 
-mongoose.connect(process.env.MONGO_URL).then(()=>{
-    console.log('Base de datos conectada')
-})
-.catch((err)=>{
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Base de datos conectada");
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 const app = express();
 
 app.use(express.json());
 
-app.listen(3000, ()=>{
-    console.log('Probando server en el puerto 3000 xd')
-})
+app.listen(3000, () => {
+  console.log("Probando server en el puerto 3000 xd");
+});
 
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
+// middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    succes: false,
+    statusCode,
+    message
+  });
+});
