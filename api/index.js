fcde1dcd97effc7acb46b3cc863d082046f749db
Santiago Andrade Mesa <andrade11mesa@gmail.com>
2024-06-10 22:08:16 -0500

    Change lenguage mssg error

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ app.use("/api/auth", authRoutes);
 // middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
+  const message = err.message || "Error interno en el servidor";
   res.status(statusCode).json({
     succes: false,
     statusCode,
