969086e5f1ebef76b45a81120b03c7b8042ee463
Santiago Andrade Mesa <andrade11mesa@gmail.com>
2024-06-26 15:37:34 -0500

    Create functionality update user with conditions

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,67 @@
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
 
 export const test = (req, res) => {
-  res.json({ message: "Works" });
+  res.json({ message: 'Works' });
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'No autorizado a actualizar este usuario'));
+  }
+  if (req.body.password) {
+    if (req.body.password.length < 6) {
+      return next(
+        errorHandler(400, 'La contraseña debe tener al menos 6 caracteres')
+      );
+    }
+    req.body.password = bcryptjs.hashSync(req.body.password, 10);
+  }
+  if (req.body.username) {
+    if (req.body.username.length < 7 || req.body.username.length > 20) {
+      return next(
+        errorHandler(
+          400,
+          'El nombre de usuario debe tener entre 7 y 20 caracteres'
+        )
+      );
+    }
+  }
+  if (req.body.username.includes(' ')) {
+    return next(
+      errorHandler(400, 'El nombre de usuario no puede tener espacios')
+    );
+  }
+  if (req.body.username !== req.body.username.toLowerCase()) {
+    return next(
+      errorHandler(400, 'El nombre de usuario debe estar en minúsculas')
+    );
+  }
+  if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+    return next(
+      errorHandler(
+        400,
+        'El nombre de usuario solo puede tener letras y números'
+      )
+    );
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          profilePicture: req.body.profilePicture,
+          password: req.body.password,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
