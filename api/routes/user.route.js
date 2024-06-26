41acbe4186586f3f00ffd37890c49cb8f2f3bd66
Santiago Andrade Mesa <andrade11mesa@gmail.com>
2024-06-26 15:38:02 -0500

    Add route to update user verifyToken with id

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,11 @@
-import { Router } from "express";
-import { test } from "../controllers/user.controller.js";
+import { Router } from 'express';
+import { test, updateUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = Router();
 
 router.get('/test', test);
 
+router.put('/update/:userId', verifyToken, updateUser);
 
-export  default router;
+export default router;
