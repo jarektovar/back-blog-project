170a62360bd38ed90296d24553fdb63b403ca99f
Santiago Andrade Mesa <andrade11mesa@gmail.com>
2024-06-27 14:19:26 -0500

    Add functionality to deleteUser

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -65,3 +65,15 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'No autorizado a eliminar este usuario'));
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json({ message: 'Usuario eliminado' });
+  } catch (error) {
+    next(error);
+  }
+};
