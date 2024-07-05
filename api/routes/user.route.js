40306bd7091fefcca34ea1b1e895b53388c3a693
Jazmin-Moreno <jazmin.moreno01@uptc.edu.co>
2024-07-05 01:12:08 -0500

    router y controller getUsers

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import {
   deleteUser,
+  getUsers,
   signout,
   test,
   updateUser,
@@ -10,11 +11,9 @@ import { verifyToken } from '../utils/verifyUser.js';
 const router = Router();
 
 router.get('/test', test);
-
 router.put('/update/:userId', verifyToken, updateUser);
-
 router.delete('/delete/:userId', verifyToken, deleteUser);
-
 router.post('/signout', signout);
+router.get('/getusers', verifyToken, getUsers);
 
 export default router;
