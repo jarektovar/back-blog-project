d4eae1d225137ad51107812289a1d81f0a3d3c54
SantiagoSalamancaa <bolthgg@gmail.com>
2024-07-25 01:33:52 -0500

    config delete button on post

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -81,3 +81,21 @@ export const editComment = async (req, res, next) => {
         next(error);
     }
 };
+
+export const deleteComment = async (req, res, next) => {
+    try {
+      const comment = await Comment.findById(req.params.commentId);
+      if (!comment) {
+        return next(errorHandler(404, 'Comentario no encontrado.'));
+      }
+      if (comment.userId !== req.user.id && !req.user.isAdmin) {
+        return next(
+          errorHandler(403, 'No tienes acceso.')
+        );
+      }
+      await Comment.findByIdAndDelete(req.params.commentId);
+      res.status(200).json('Comentario borrado con exito.');
+    } catch (error) {
+      next(error);
+    }
+  };
