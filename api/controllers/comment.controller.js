e759136f173f597780cb74873bb66d6d63dff9cf
SantiagoSalamancaa <bolthgg@gmail.com>
2024-07-25 00:42:02 -0500

    config add edit button on post

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -1,33 +1,33 @@
 import { errorHandler } from "../utils/error.js";
 import Comment from "../models/comment.model.js"
 
-export const createComment = async (req, res, next) =>{
-    try{
-        const{content, postId, userId} = req.body;
+export const createComment = async (req, res, next) => {
+    try {
+        const { content, postId, userId } = req.body;
 
-        if(userId !== req.user.id){
-            return next(errorHandler(403,'No tienes permitido subir este comentario'));   
+        if (userId !== req.user.id) {
+            return next(errorHandler(403, 'No tienes permitido subir este comentario'));
         }
 
-        const newComment = new Comment ({
+        const newComment = new Comment({
             content,
             postId,
             userId,
-           /*  likes,
-            numberOfLikes */
+            /*  likes,
+             numberOfLikes */
         });
         await newComment.save();
         res.status(200).json(newComment);
 
 
-    } catch(error){
+    } catch (error) {
         next(error);
     }
 };
 
-export const getPostComments= async(req, res, next)=>{
+export const getPostComments = async (req, res, next) => {
     try {
-        const comments = await Comment.find({postId: req.params.postId}).sort({
+        const comments = await Comment.find({ postId: req.params.postId }).sort({
             createdAt: -1,
         });
         res.status(200).json(comments);
@@ -44,15 +44,40 @@ export const likeComment = async (req, res, next) => {
         }
         const userIndex = comment.likes.indexOf(req.user.id);
         if (userIndex === -1) {
-            comment.numberOfLikes +=1;
+            comment.numberOfLikes += 1;
             comment.likes.push(req.user.id);
         } else {
-            comment.numberOfLikes -=1;
+            comment.numberOfLikes -= 1;
             comment.likes.splice(userIndex, 1);
         }
         await comment.save();
         res.status(200).json(comment);
     } catch (error) {
         next(error);
-    }   
-}
+    }
+};
+
+export const editComment = async (req, res, next) => {
+    try {
+        const comment = await Comment.findById(req.params.commentId);
+        if (!comment) {
+            return next(errorHandler(404, 'Comentario no encontrado'));
+        }
+        if (comment.userId !== req.user.id && !req.user.isAdmin) {
+            return next(
+                errorHandler(403, 'Permiso denegado')
+            );
+        }
+
+        const editedComment = await Comment.findByIdAndUpdate(
+            req.params.commentId,
+            {
+                content: req.body.content,
+            },
+            { new: true }
+        );
+        res.status(200).json(editedComment);
+    } catch (error) {
+        next(error);
+    }
+};
